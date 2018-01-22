a53dcff45305d8efe1eedc6dd0421c97d10c9ff8
wbear <965222938@qq.com>
2018-01-22 09:38:59 +0800

    Revert "class2"
    This reverts commit c70821017f074a4162b609f9260dc46946d527ff.

--- Python3网络爬虫入门(Jack Cui)/class2.py
+++ Python3网络爬虫入门(Jack Cui)/class2.py
@@ -1,30 +1,11 @@
 #！/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-# -*- coding: UTF-8 -*-
 from urllib import request
-from urllib import parse
-import json
-
 if __name__ == "__main__":
-    #对应上图的Request URL
-    Request_URL = 'http://fanyi.baidu.com/v2transapi'
-    #创建Form_Data字典，存储上图的Form Data
-    Form_Data = {}
-    Form_Data['from'] = 'en'
-    Form_Data['to'] = 'zh'
-    Form_Data['query'] = 'zhouqi'
-    Form_Data['transtype'] = 'enter'
-    Form_Data['simple_means_flag'] = '3'
-
-    #使用urlencode方法转换标准格式
-    data = parse.urlencode(Form_Data).encode('utf-8')
-    #传递Request对象和转换完格式的数据
-    response = request.urlopen(Request_URL,data)
-    #读取信息并解码
-    html = response.read().decode('utf-8')
-    #使用JSON
-    translate_results = json.loads(html)
-    #找到翻译结果
-    ##translate_results = translate_results['translateResult'][0][0]['tgt']
-    #打印翻译信息
-    print("翻译的结果是：%s" % translate_results)
+    req = request.Request("http://fanyi.baidu.com/")
+    response = request.urlopen(req)
+    print("geturl打印信息：%s"%(response.geturl()))
+    print('**********************************************')
+    print("info打印信息：%s"%(response.info()))
+    print('**********************************************')
+    print("getcode打印信息：%s"%(response.getcode()))
