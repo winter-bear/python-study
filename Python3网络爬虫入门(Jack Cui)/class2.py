4c688c33965713df0e330432678f7de6e25eeb67
wbear <965222938@qq.com>
2018-01-23 15:13:27 +0800

    class 2 py
    class 2 py

--- Python3网络爬虫入门(Jack Cui)/class2.py
+++ Python3网络爬虫入门(Jack Cui)/class2.py
@@ -45,5 +45,46 @@ if __name__ == '__main__':
         content = input()  
         if (content == 'q'):  
             break  
-        baidu_translate(content)  
+        baidu_translate(content)
+'''
+百度翻译
+import http
+import hashlib
+import urllib.request
+import random
+import json
+
+while True:
+    fin = open(r'D:\1.txt', 'r')               #以读的方式打开输入文件  
+    fout = open(r'D:\2.txt', 'w')             #以写的方式打开输出文件
+    for eachLine in fin:
+        appid = '自己申请'    #参考百度翻译后台，申请appid和secretKey
+        secretKey = '自己申请'
+        httpClient = None
+        myurl = '/api/trans/vip/translate'
+        q = eachLine.strip()                   #文本文件中每一行作为一个翻译源
+        fromLang = 'zh'                         #中文
+        toLang = 'en'                             #英文
+        salt = random.randint(32768, 65536)
+        sign = appid+q+str(salt)+secretKey
+        sign = sign.encode('UTF-8')
+        m1 = hashlib.md5()
+        m1.update(sign)
+        sign = m1.hexdigest()
+        myurl = myurl+'?appid='+appid+'&q='+urllib.parse.quote(q)+'&from='+fromLang+'&to='+toLang+'&salt='+str(salt)+'&sign='+sign
+        httpClient = http.client.HTTPConnection('api.fanyi.baidu.com')
+        httpClient.request('GET', myurl)
+        #response是HTTPResponse对象
+        response = httpClient.getresponse()
+        html= response.read().decode('UTF-8')
+        target2 = json.loads(html)
+        src = target2["trans_result"][0]["dst"]
+        #print(src)#取得翻译后的文本结果,测试可删除注释  
+        outStr = src  
+        fout.write(outStr.strip() + '\n')  
+    fin.close()  
+    fout.close()
+    print('翻译成功，请查看文件')
+    break
+‘’‘
 
